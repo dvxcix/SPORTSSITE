ba87af0bbd1f139edfd22123a12b6a724841aa0a
dvxcix <dev.vick@protonmail.com>
2026-09-16 04:37:52 -0400

    Harden research workspace RPC access

---
+++ hub/supabase/migrations/20260916083700_harden_research_workspace_rpcs.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.invite_research_workspace_member(uuid, text, text) from anon;
+revoke execute on function public.remove_research_workspace_member(uuid, uuid) from anon;
+revoke execute on function public.save_research_workspace_board(uuid, uuid[], uuid[], uuid[]) from anon;
